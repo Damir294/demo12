59daffd2246172ebe6869163366d7655907fcd63
Damir294 <117120261+Damir294@users.noreply.github.com>
2023-02-08 00:30:14 +0400

    JUnit-Mockito

---
+++ src/main/java/repository/UserRepository.java
@@ -0,0 +1,16 @@
+package repository;
+
+import java.util.Collection;
+import java.util.Optional;
+
+public interface UserRepository {
+    void addUser(User user);
+
+    void clearUsers();
+
+    Collection<User> getAllUsers();
+
+    Optional<User> findByLogin(String login);
+
+    Optional<User> findByLoginAndPassword(String login, String password);
+}
